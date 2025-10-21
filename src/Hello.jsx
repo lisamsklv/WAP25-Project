fb28adc97b634ede305d202a9142e01b50aef3d3
fmboaintz <s2310238055@fhooe.at>
2025-10-21 10:14:23 +0200

    Added ue3

---
+++ src/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+    return <p>Hello world!</p>;
+}
+
+export default Hello;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App.jsx';
+import './index.css';
+import User from './User.jsx';
+
+// Beispiel-Funktion zum Erstellen eines Users
+function createUser(name, email) {
+  return <User name={name} email={email} />;
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "c",
+    element: <h1>This is possible, too</h1>,
+  },  
+  {
+    path: "user/:userId",
+    element: createUser("Max Mustermann", "max@beispiel.de"),
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
