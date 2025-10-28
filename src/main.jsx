624928954ab40efbebe387fcebb4113876044c81
fmboaintz <s2310238055@fhooe.at>
2025-10-28 09:00:53 +0100

    Added login page, and registration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+
 import './index.css'
 import App from './App.jsx'
+import Login from './pages/LoginPage.jsx'
+import Registration from './pages/RegistrationPage.jsx'
 
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+
 import en from './en.json';
 import de from './de.json';
 
@@ -12,8 +18,21 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  }, 
+
+  {
+    path: "/login",
+    element: <Login />
+  }, 
+
+  {
+    path: "/registration",
+    element: <Registration />
   },
+
+
 ]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
