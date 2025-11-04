16f7c52fc2fbbd30e359f6082a262d121f840eee
fmboaintz <s2310238055@fhooe.at>
2025-11-04 13:20:46 +0100

    UE5

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 import express from 'express';
+import api from "./backend_src/api.js";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config';
+
 
 const app = express();
 const port = 3000;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-
-// route definitions
+const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
 
 app.use(express.json());
 
-app.get('/api', (req, res) => {
-    res.json({
-        success: true,
-    });
-});
+// backend routes
+app.use('/api', api);
+
 
 app.use(express.static('dist'));
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
-// start server
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+try {
+  const client = new MongoClient(connectionString);
+  await client.connect();
+  const db = client.db('demo');
+
+  app.set('db', db); // save a reference to the db to app config
+
+  // start server
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+} catch (err) {
+  console.error(err);
+}
 
---
+++ backend_src/api.js
@@ -0,0 +1,104 @@
+import express from 'express';
+import { ObjectId } from 'mongodb';
+
+const router = express.Router();
+
+router.get('/user', async (req, res) => {
+  try {
+    const db = req.app.get('db'); // get reference to the db from app config
+    const users = await db.collection('users').find({}).toArray();
+
+    res.json(users);
+  } catch(err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.get('/user/:id', async (req, res) => {
+  try {
+    const db = req.app.get('db');
+    const user = await db.collection('users').findOne({ _id: new ObjectId(req.params.id) });
+
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).send();
+    }
+  } catch(err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+
+router.post('/user', async (req, res) => {
+  try {
+    const db = req.app.get('db');
+    console.log(JSON.stringify(req.body, null, '\n'));
+    const insertion = await db.collection('users').insertOne(req.body);
+    if (insertion.acknowledged) {
+      const user = await db.collection('users')
+        .findOne({ _id: insertion.insertedId });
+
+      if (user) {
+        res.status(201).json(user);
+      } else {
+        res.status(404).send();
+      }
+    } else {
+      res.status(500).send();
+    }
+  } catch(err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.put('/user/:id', async (req, res) => {
+  try {
+    const db = req.app.get('db');
+
+    const updateData = req.body;
+    delete updateData._id;
+
+    const updated = await db.collection('users')
+      .updateOne({ _id: new ObjectId(req.params.id) }, { $set: updateData });
+
+    if (updated.modifiedCount === 1) {
+      const user = await db.collection('users')
+        .findOne({ _id: new ObjectId(req.params.id) });
+
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).send();
+      }
+    } else {
+      res.status(404).send();
+    }
+  } catch(err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+
+router.delete('/user/:id', async (req, res) => {
+  try {
+    const db = req.app.get('db');
+    const deleted = await db.collection('users')
+      .deleteOne({ _id: new ObjectId(req.params.id) });
+
+    if (deleted.deletedCount === 1) {
+      res.send();
+    } else {
+      res.status(404).send();
+    }
+  } catch(err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+export default router;
