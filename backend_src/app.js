9b25b3a8cbb81479eb01d53ce3c9c0492c1a9f2b
fmboaintz <s2310238055@fhooe.at>
2025-11-11 00:01:29 +0100

    more cleaning up

--- backend_src/app.js
+++ backend_src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import api from "./backend_src/api.js";
+import api from "./api.js";
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { MongoClient } from 'mongodb';
@@ -9,20 +9,44 @@ import 'dotenv/config';
 const app = express();
 const port = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
 
+
+// ===========================================
+//             GLOBALE MIDDLEWARE
+// ===========================================
+
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`[LOG] ${new Date().toISOString()} - ${req.method}: ${req.url}`); 
+  next();
+});
+
+
+// ===========================================
+//          ROUTEN UND STATIC FILES
+// ===========================================
+
 
 // backend routes
 app.use('/api', api);
 
-
 app.use(express.static('dist'));
+
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, './dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
+
+// ===========================================
+//       SERVER START UND DB-VERBINDUNG
+// ===========================================
+
 
 try {
   await client.connect();
